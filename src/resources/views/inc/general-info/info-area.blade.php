a05b773e5a78d68798c39bf3ab9171d2b014c661
AleksiyD <50235593+AleksiyD@users.noreply.github.com>
2024-05-28 17:46:02 +0300

    added general info

---
+++ src/resources/views/inc/general-info/info-area.blade.php
@@ -0,0 +1,9 @@
+<section class="schedule">
+    <div class="container">
+        <div class="schedule-inner">
+            <div class="row">
+                {{ $slot }}
+            </div>
+        </div>
+    </div>
+</section>
---
+++ src/resources/views/inc/general-info/info-card.blade.php
@@ -0,0 +1,11 @@
+<div class="col-lg-4 col-md-6 col-12 ">
+    <!-- single-schedule -->
+    <div class="single-schedule h-100">
+        <div class="inner">
+            <div class="single-content">
+                <h4>{{ $title }}</h4>
+                <p>{{ $text }}</p>
+            </div>
+        </div>
+    </div>
+</div>
